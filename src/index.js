d2fe91c6d1f16040ca22ce76d6cbdbdf9b340db1
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-04-04 18:45:04 +0100

    Update CSS and HTML files, add new products to items.json

--- src/index.js
+++ src/index.js
@@ -9,46 +9,17 @@ async function fetchItems() {
     }
 }
 
-const products = [
-    {
-        title: `MacBook Air 13" 2023 - Midnight`,
-        description: `The new MacBook Air. Lightness strikes again.`,
-        productId: `macbook_air_13_midnight`,
-        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mba13-midnight-config-202402?wid=820&hei=498&fmt=jpeg&qlt=90&.v=1708371033110',
-    },
-
-    {
-        title: `MacBook Pro 16" 2023 - Space Gray`,
-        description: `The new MacBook Pro. Power. Moves.`,
-        productId: `macbook_pro_16_space_gray`,
-        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spacegray-select-202110?wid=904&hei=840&fmt=jpeg&qlt=95&.v=1634217871000',
-    },
-
-    {
-        title: `iPhone 14 Pro - Graphite`,
-        description: `The new iPhone 14 Pro. Pro camera. Pro display. Pro performance.`,
-        productId: `iphone_14_pro_graphite`,
-        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-pro-graphite-select-202209?wid=940&hei=1112&fmt=jpeg&qlt=95&.v=1662726826000',
-    },
-
-    {
-        title: `iPad Pro 12.9" 2023 - Silver`,
-        description: `The new iPad Pro. Your next computer is not a computer.`,
-        productId: `ipad_pro_12_9_silver`,
-        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-12-select-202104?wid=940&hei=1112&fmt=jpeg&qlt=95&.v=1616704248000',
-    },
-
-    {
-        title: `Apple Watch Series 8 - Blue`,
-        description: `The new Apple Watch Series 8. The future of health is on your wrist.`,
-        productId: `apple_watch_series_8_blue`,
-        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/aw-s8-blue-nc-202109?wid=940&hei=1112&fmt=jpeg&qlt=95&.v=1631636188000',
-    },
-];
-
+const products_top = [];
 let currentProductIndex = 0;
 const updateProduct = () => {
-    const product = products[currentProductIndex];
+    console.log('Updating product...', currentProductIndex);
+    const product = products_top[currentProductIndex];
+
+    console.log(product);
+
+    if (!product) {
+        return;
+    }
 
     const parentDiv = document.getElementById('data_home_top_products');
     const contentDivs = parentDiv.querySelectorAll('.flex');
@@ -61,15 +32,15 @@ const updateProduct = () => {
         const productIdElement = document.querySelector('[data-top-product-product_id]');
         const imageElement = document.querySelector('[data-top-product-image]');
 
-        titleElement.setAttribute('data-top-product-title', product.title);
-        descriptionElement.setAttribute('data-top-product-description', product.description);
-        productIdElement.setAttribute('data-top-product-product_id', product.productId);
+        titleElement.setAttribute('data-top-product-title', product.product_name);
+        descriptionElement.setAttribute('data-top-product-description', product.product_description);
+        productIdElement.setAttribute('data-top-product-product_id', product.product_id);
 
-        titleElement.innerHTML = product.title
-        descriptionElement.innerHTML = product.description
-        productIdElement.href = `product.html?product_id=${product.productId}`;
+        titleElement.innerHTML = product.product_name
+        descriptionElement.innerHTML = product.product_description
+        productIdElement.href = `product.html?product_id=${product.product_id}`;
 
-        imageElement.setAttribute('src', product.image);
+        imageElement.setAttribute('src', product.product_image);
 
         contentDivs.forEach(div => {
             div.classList.remove('slide-out');
@@ -78,24 +49,25 @@ const updateProduct = () => {
 
         setTimeout(() => {
             contentDivs.forEach(div => div.classList.remove('slide-in'));
-            currentProductIndex = (currentProductIndex + 1) % products.length;
+            currentProductIndex = (currentProductIndex + 1) % products_top.length;
         }, 500); // Remove the class after 0.5s, which is the duration of the animation
 
     }, 500);
 }
 
-let dataItems = [];
-
 document.addEventListener('DOMContentLoaded', () => {
     fetchItems().then((data) => {
-        dataItems.push(...data);
-    });
+        if (data.length === 0) {
+            return;
+        }
 
-    dataItems.forEach((item) => {
-        console.log(item);
+        Object.entries(data).forEach(([key, value]) => {
+            if (value['top_seller']) {
+                products_top.push(value);
+            }
+        });
+    }).then(() => {
+        updateProduct();
+        setInterval(updateProduct, 5000); // Change product every 5 seconds
     });
-
-    // Update product every 5 seconds and at the beginning
-    updateProduct();
-    setInterval(updateProduct, 5000); // Change product every 5 seconds
 });
