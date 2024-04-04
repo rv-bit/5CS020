18ad20b891454113835a5a0953c6ec80aab59ddb
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-04-04 17:43:21 +0100

    Update CSS and HTML for top product section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-let isDown = false;
-let startX;
-let scrollLeft;
-
 async function fetchItems() {
     try {
         const response = await fetch('assets/items.json');
@@ -13,53 +9,68 @@ async function fetchItems() {
     }
 }
 
-const smoothScroll = (target, scrollLeft) => {
-    let start = null;
+const products = [
+    {
+        title: `MacBook Air 13" 2023 - Midnight`,
+        description: `The new MacBook Air. Lightness strikes again.`,
+        productId: `macbook_air_13_midnight`,
+        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mba13-midnight-config-202402?wid=820&hei=498&fmt=jpeg&qlt=90&.v=1708371033110',
+    },
 
-    function step(timestamp) {
-        if (!start) start = timestamp;
-        const progress = timestamp - start;
-        const result = Math.min(scrollLeft + progress / 1000 * 300, target.scrollLeft);
-        target.scrollLeft = result;
-        if (result < target.scrollLeft) {
-            window.requestAnimationFrame(step);
-        }
-    }
+    {
+        title: `MacBook Pro 16" 2023 - Space Gray`,
+        description: `The new MacBook Pro. Power. Moves.`,
+        productId: `macbook_pro_16_space_gray`,
+        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spacegray-select-202110?wid=904&hei=840&fmt=jpeg&qlt=95&.v=1634217871000',
+    },
 
-    window.requestAnimationFrame(step);
+    {
+        title: `iPhone 14 Pro - Graphite`,
+        description: `The new iPhone 14 Pro. Pro camera. Pro display. Pro performance.`,
+        productId: `iphone_14_pro_graphite`,
+        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-pro-graphite-select-202209?wid=940&hei=1112&fmt=jpeg&qlt=95&.v=1662726826000',
+    },
+
+    {
+        title: `iPad Pro 12.9" 2023 - Silver`,
+        description: `The new iPad Pro. Your next computer is not a computer.`,
+        productId: `ipad_pro_12_9_silver`,
+        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-12-select-202104?wid=940&hei=1112&fmt=jpeg&qlt=95&.v=1616704248000',
+    },
+
+    {
+        title: `Apple Watch Series 8 - Blue`,
+        description: `The new Apple Watch Series 8. The future of health is on your wrist.`,
+        productId: `apple_watch_series_8_blue`,
+        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/aw-s8-blue-nc-202109?wid=940&hei=1112&fmt=jpeg&qlt=95&.v=1631636188000',
+    },
+];
+
+let currentProductIndex = 0;
+const updateProduct = () => {
+    const product = products[currentProductIndex];
+
+    const titleElement = document.querySelector('[data-top-product-title]');
+    const descriptionElement = document.querySelector('[data-top-product-description]');
+    const productIdElement = document.querySelector('[data-top-product-product_id]');
+    const imageElement = document.querySelector('[data-top-product-image]');
+
+    titleElement.setAttribute('data-top-product-title', product.title);
+    descriptionElement.setAttribute('data-top-product-description', product.description);
+    productIdElement.setAttribute('data-top-product-product_id', product.productId);
+
+    titleElement.innerHTML = product.title
+    descriptionElement.innerHTML = product.description
+    productIdElement.href = `product.html?product_id=${product.productId}`;
+
+    imageElement.setAttribute('src', product.image);
+
+    currentProductIndex = (currentProductIndex + 1) % products.length;
 }
 
+let dataItems = [];
+
 document.addEventListener('DOMContentLoaded', () => {
-    const slider = document.getElementById('scrollable-div');
-
-    slider.addEventListener('mousedown', (e) => {
-        isDown = true;
-        slider.classList.add('active');
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-
-        smoothScroll(slider, slider.scrollLeft);
-    });
-
-    slider.addEventListener('mouseleave', () => {
-        isDown = false;
-        slider.classList.remove('active');
-    });
-
-    slider.addEventListener('mouseup', () => {
-        isDown = false;
-        slider.classList.remove('active');
-    });
-
-    slider.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const walk = (x - startX) * 1; //scroll-fast
-        slider.scrollLeft = scrollLeft - walk;
-    });
-
-    const dataItems = [];
     fetchItems().then((data) => {
         dataItems.push(...data);
     });
@@ -67,4 +78,8 @@ document.addEventListener('DOMContentLoaded', () => {
     dataItems.forEach((item) => {
         console.log(item);
     });
+
+    // Update product every 5 seconds and at the beginning
+    updateProduct();
+    setInterval(updateProduct, 5000); // Change product every 5 seconds
 });
