9c2f045bcf8ce3bb7056e332128728a7af9da0a5
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-04-11 17:58:32 +0100

    refactor: CSS and JavaScript code add: new updates to product page, menu fixed: responsive issues

--- src/index.js
+++ src/index.js
@@ -55,36 +55,6 @@ const updateProduct = () => {
     }, 500);
 }
 
-const openMenu = () => {
-    const menu = document.getElementById('menu_dropdown');
-    const body = document.querySelector('body');
-
-    if (menu.classList.contains('flex')) {
-        menu.classList.add('slide-top');
-
-        setTimeout(() => {
-            menu.classList.remove('slide-top');
-
-            menu.classList.add('hidden');
-            menu.classList.remove('flex');
-        }, 500); // Remove the class after 0.5s, which is the duration of the animation
-
-        // Enable scrolling
-        body.style.overflow = '';
-        return;
-    }
-
-    body.style.overflow = 'hidden';
-
-    menu.classList.add('flex');
-    menu.classList.remove('hidden');
-
-    menu.classList.add('slide-bottom');
-    setTimeout(() => {
-        menu.classList.remove('slide-bottom');
-    }, 500); // Remove the class after 0.5s, which is the duration of the animation
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     fetchItems().then((data) => {
         if (!data) return;
--- src/pages/products.js
+++ src/pages/products.js
@@ -13,6 +13,32 @@ const adjustWidthSelectors = (selectedElement) => {
     document.body.removeChild(tempElement);
 }
 
+const closeOptions = (selectElement) => {
+    var children = selectElement.children;
+    var childOfChildSVG = children[0].children;
+    var childOfChildContent = children[1];
+
+    if (childOfChildSVG[2] === undefined) {
+        childOfChildSVG = childOfChildSVG[0].children;
+    }
+
+    if (childOfChildContent === undefined) {
+        childOfChildContent = children[0].children[1];
+    }
+
+    if (childOfChildSVG[1].classList.contains('hidden')) {
+        childOfChildSVG[1].classList.remove('hidden');
+        childOfChildSVG[2].classList.add('hidden');
+
+        childOfChildContent.classList.add('hidden');
+    } else {
+        childOfChildSVG[1].classList.add('hidden');
+        childOfChildSVG[2].classList.remove('hidden');
+
+        childOfChildContent.classList.remove('hidden');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     window.onload = function () {
         var selectElement = document.getElementById('sort');
