8491cace3aac43aa485d95656dab78ce43d515b3
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-04-11 19:10:27 +0100

    refactor: filter menu in products page refactor: js code in products page added: css code in products page

--- src/pages/products.js
+++ src/pages/products.js
@@ -39,6 +39,36 @@ const closeOptions = (selectElement) => {
     }
 }
 
+const openFilterMenu = () => {
+    const element = document.getElementById('menu_dropdown_filters');
+    const body = document.querySelector('body');
+
+    if (element.classList.contains('flex')) {
+        element.classList.add('slide-top');
+
+        setTimeout(() => {
+            element.classList.remove('slide-top');
+
+            element.classList.add('hidden');
+            element.classList.remove('flex');
+        }, 500); // Remove the class after 0.5s, which is the duration of the animation
+
+        // Enable scrolling
+        body.style.overflow = '';
+        return;
+    }
+
+    body.style.overflow = 'hidden';
+
+    element.classList.add('flex');
+    element.classList.remove('hidden');
+
+    element.classList.add('slide-bottom');
+    setTimeout(() => {
+        menu.classList.remove('slide-bottom');
+    }, 500); // Remove the class after 0.5s, which is the duration of the animation
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     window.onload = function () {
         var selectElement = document.getElementById('sort');
