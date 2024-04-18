c4d87833a75ad519726ab618330b512d53a3b199
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-04-18 18:34:06 +0100

    feat: added google locations api to get addresses added: dynamic inputs for address inputs fixed: height overflow for the information

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,3 +1,73 @@
+const inputFormFields = [
+    {
+        id: 'card-card-number"',
+        type: 'text',
+        placeholder: 'xxxx xxxx xxxx xxxx',
+        required: true,
+
+        requiredText: 'Card number is required',
+    },
+    {
+        id: 'card-expiry-date',
+        type: 'text',
+        placeholder: 'MM/YY',
+        required: true,
+
+        requiredText: 'Expiry date is required',
+    },
+    {
+        id: 'card-cvv',
+        type: 'text',
+        placeholder: 'xxx',
+        required: true,
+
+        requiredText: 'CVV is required',
+    },
+    {
+        id: 'email',
+        type: 'email',
+        placeholder: 'example@gmail.com',
+        required: true,
+
+        requiredText: 'Email is required',
+    },
+]
+
+const inputAddressFields = [
+    {
+        id: 'post_code',
+        type: 'text',
+        placeholder: 'WV10 0AU',
+        required: true,
+
+        requiredText: 'Post code is required',
+    },
+    {
+        id: 'house_number',
+        type: 'text',
+        placeholder: 'House Number',
+        required: true,
+
+        requiredText: 'Address is required',
+    },
+    {
+        id: 'city',
+        type: 'text',
+        placeholder: 'City',
+        required: true,
+
+        requiredText: 'City is required',
+    },
+    {
+        id: 'country',
+        type: 'text',
+        placeholder: 'United Kingdom',
+        required: true,
+
+        requiredText: 'Country is required',
+    },
+]
+
 let checkoutProducts = [];
 const createCheckoutItems = (cartItems) => {
     if (cartItems.length <= 0) {
@@ -76,6 +146,50 @@ const createCheckoutItems = (cartItems) => {
     });
 }
 
+const checkoutPayment = () => {
+    var section = document.querySelector('section[role="form-section"]');
+    var inputs = section.querySelectorAll('input');
+
+    const inputsArrayOfNotFilled = [];
+
+    inputs.forEach(function (input) {
+        if (input.required) {
+            console.log(input, 'required input'); // Logs the value of each required input element
+
+            input.addEventListener('input', function (e) {
+                console.log(e.target.value, 'input value'); // Logs the value of the input element
+
+                if (e.target.value === '' || e.target.value === null || e.target.value === undefined) {
+                    e.target.parentElement.querySelector('p').classList.remove('hidden');
+                    e.target.parentElement.querySelector('p').classList.add('flex');
+
+                    console.log(e.target, 'input target'); // Logs the input element
+
+                    inputsArrayOfNotFilled.push(e.target);
+                } else {
+                    e.target.parentElement.querySelector('p').classList.add('hidden');
+                    e.target.parentElement.querySelector('p').classList.remove('flex');
+                }
+            });
+        }
+
+        if (input.type === 'checkbox') {
+            console.log(input.checked, 'checkbox for billing'); // Logs whether the checkbox is checked
+        } else {
+            console.log(input.value); // Logs the value of each input element
+        }
+    });
+
+    if (inputsArrayOfNotFilled.length > 0) {
+        return;
+    }
+
+    // window.location.href = 'cart.html';
+
+    // If all inputs are filled, then proceed to the next step
+    console.log('All inputs are filled');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
     const cartAmount = cartItems.length;
@@ -110,6 +224,128 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         window.location.href = '../pages/cart.html';
     }
+
+    document.querySelector('[role="card-card-number"').addEventListener('input', function (e) {
+        var target = e.target,
+            value = target.value;
+        start = target.selectionStart,
+            end = target.selectionEnd;
+
+        // Remove all whitespace characters from the input
+        value = value.replace(/\s/g, '');
+        value = value.replace(/\D/g, '');
+        if (value.length > 16) {
+            value = value.substring(0, 16); // Truncate to 16 digits if more are entered
+
+            document.querySelector('[role="card-card-number"').parentElement.querySelector('p').classList.add('hidden');
+            document.querySelector('[role="card-card-number"').parentElement.querySelector('p').classList.remove('remove');
+        } else {
+            document.querySelector('[role="card-card-number"').parentElement.querySelector('p').classList.add('flex');
+            document.querySelector('[role="card-card-number"').parentElement.querySelector('p').classList.remove('hidden');
+        }
+
+        // Add a space after every 4 digits
+        target.value = value.replace(/(.{4})/g, '$1 ').trim();
+
+        // Adjust the cursor position
+        if (value.length % 4 === 0 && start === value.length) {
+            start++;
+        }
+        target.setSelectionRange(start, end);
+    });
+
+    document.querySelector('[role="card-expiry-date"').addEventListener('input', function (e) {
+        var target = e.target,
+            value = target.value;
+        start = target.selectionStart,
+            end = target.selectionEnd;
+
+        // Remove all non-digit characters
+        var digits = value.replace(/\D/g, '');
+        if (digits.length > 4) {
+            digits = digits.substring(0, 4); // Truncate to 4 digits if more are entered
+
+            document.querySelector('[role="card-expiry-date"').parentElement.querySelector('p').classList.add('hidden');
+            document.querySelector('[role="card-expiry-date"').parentElement.querySelector('p').classList.remove('remove');
+        } else {
+            document.querySelector('[role="card-expiry-date"').parentElement.querySelector('p').classList.add('flex');
+            document.querySelector('[role="card-expiry-date"').parentElement.querySelector('p').classList.remove('hidden');
+        }
+
+        if (digits.length > 2) {
+            // If there are more than 2 digits, add a slash between the second and third digits
+            target.value = digits.substring(0, 2) + '/' + digits.substring(2);
+        } else {
+            target.value = digits;
+        }
+
+        // Adjust the cursor position
+        if (digits.length === 2 && start === digits.length) {
+            start++;
+        }
+        target.setSelectionRange(start, end);
+    });
+
+    document.querySelector('[role="card-cvv"').addEventListener('input', function (e) {
+        var target = e.target,
+            value = target.value;
+
+        // Remove all non-digit characters  
+        target.value = value.replace(/\D/g, '');
+
+        if (value.length > 3) {
+            target.value = value.substring(0, 3); // Truncate to 3 digits if more are entered
+
+            document.querySelector('[role="card-cvv"').parentElement.querySelector('p').classList.add('hidden');
+            document.querySelector('[role="card-cvv"').parentElement.querySelector('p').classList.remove('remove');
+        } else {
+            document.querySelector('[role="card-cvv"').parentElement.querySelector('p').classList.add('flex');
+            document.querySelector('[role="card-cvv"').parentElement.querySelector('p').classList.remove('hidden');
+        }
+    });
+
+    document.querySelector('[role="address_search"]').addEventListener('input', function (e) {
+        if (e.target.value === '' || e.target.value === null || e.target.value === undefined) {
+            document.querySelector('[role="address_information"]').classList.add('hidden');
+            document.querySelector('[role="address_information"]').classList.remove('flex');
+        }
+    });
+
+    document.querySelector('[role="address_search_billing"]').addEventListener('input', function (e) {
+        if (e.target.value === '' || e.target.value === null || e.target.value === undefined) {
+            document.querySelector('[role="address_information_billing"]').classList.add('hidden');
+            document.querySelector('[role="address_information_billing"]').classList.remove('flex');
+        }
+    });
+
+    const addressSearch = document.querySelector('[role="address_information"]');
+    const addressSearchBilling = document.querySelector('[role="address_information_billing"]');
+    inputAddressFields.forEach((field) => {
+        const div = `
+            <label class="w-full h-fit flex flex-col">
+                <input type="${field.type}" placeholder="${field.placeholder}" id="${field.id}" required="${field.required}"
+                    class="w-full flex h-9 rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50">
+
+                <p class="mt-2 hidden text-red-600">
+                    ${field.requiredText}
+                </p>
+            </label>
+        `
+
+        const div2 = `
+            <label class="w-full h-fit flex flex-col">
+                <input type="${field.type}" placeholder="${field.placeholder}" id="${field.id + '-billing'}" required="${field.required}"
+                    class="w-full flex h-9 rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50">
+
+                <p class="mt-2 hidden text-red-600">
+                    ${field.requiredText}
+                </p>
+            </label>
+        `
+
+        addressSearch.insertAdjacentHTML('beforeend', div);
+        addressSearchBilling.insertAdjacentHTML('beforeend', div2);
+    });
 
     let isDown = false;
     let startX;
