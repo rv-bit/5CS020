12cc337ddad31a3258aa815993fa4a1eb6600646
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-04-19 20:41:28 +0100

    feat: added animation to toggle from login and creating account

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,22 +1,46 @@
 const movePage = (page) => {
-    if (page === 'login') {
-        const accountLogin = document.querySelector('[role="account-login"]');
-        const accountSignup = document.querySelector('[role="account-signup"]');
+    const accountLogin = document.querySelector('[role="account-login"]');
+    const accountSignup = document.querySelector('[role="account-signup"]');
 
+    const loginDetails = accountLogin.querySelector('[role="login-details"]');
+    const loginForm = accountLogin.querySelector('[role="login-form"]');
+
+    const signUpDetails = accountSignup.querySelector('[role="signUp-details"]');
+    const signUpForm = accountSignup.querySelector('[role="signUp-form"]');
+
+    if (page === 'login') {
         accountLogin.classList.add('flex')
         accountLogin.classList.remove('hidden')
 
         accountSignup.classList.add('hidden')
         accountSignup.classList.remove('flex')
-        return;
+
+        loginForm.classList.add('slide-from-right');
+        loginDetails.classList.add('slide-from-left');
+
+        setTimeout(() => {
+            loginForm.classList.remove('slide-from-right');
+            loginDetails.classList.remove('slide-from-left');
+
+            signUpDetails.style.order = '2';
+            signUpForm.style.order = '1';
+        }, 500); // Remove the class after 0.5s, which is the duration of the animation
+    } else {
+        accountSignup.classList.add('flex')
+        accountSignup.classList.remove('hidden')
+
+        accountLogin.classList.add('hidden')
+        accountLogin.classList.remove('flex')
+
+        signUpDetails.classList.add('slide-from-right');
+        signUpForm.classList.add('slide-from-left');
+
+        signUpDetails.style.order = '1';
+        signUpForm.style.order = '2';
+
+        setTimeout(() => {
+            signUpDetails.classList.remove('slide-from-right');
+            signUpForm.classList.remove('slide-from-left');
+        }, 500); // Remove the class after 0.5s, which is the duration of the animation
     }
-
-    const accountLogin = document.querySelector('[role="account-login"]');
-    const accountSignup = document.querySelector('[role="account-signup"]');
-
-    accountSignup.classList.add('flex')
-    accountSignup.classList.remove('hidden')
-
-    accountLogin.classList.add('hidden')
-    accountLogin.classList.remove('flex')
 }
