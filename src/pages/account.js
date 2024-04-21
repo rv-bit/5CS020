63e27ec9d5f1826cbde35b9aa420c2138dfd5019
Robert Vilcu <76445558+rv-bit@users.noreply.github.com>
2024-04-21 13:41:06 +0100

    feat: created full working form for login and sign up actions, saves the users info in local storage to be used in checkout

--- src/pages/account.js
+++ src/pages/account.js
@@ -44,3 +44,168 @@ const movePage = (page) => {
         }, 500); // Remove the class after 0.5s, which is the duration of the animation
     }
 }
+
+const checkDetailsForSignUp = () => {
+    const email = document.getElementById('email-signup');
+    const password = document.getElementById('password-signup');
+    const passwordConfirmation = document.getElementById('password-confirmation');
+
+    if (email.value === '') {
+        email.parentElement.querySelector('p').classList.remove('hidden');
+        email.parentElement.querySelector('p').classList.add('block');
+        return;
+    }
+
+    if (password.value === '') {
+        password.parentElement.querySelector('p').classList.remove('hidden');
+        password.parentElement.querySelector('p').classList.add('block');
+        return;
+    }
+
+    if (passwordConfirmation.value === '') {
+        passwordConfirmation.parentElement.querySelector('p').classList.remove('hidden');
+        passwordConfirmation.parentElement.querySelector('p').classList.add('block');
+        return;
+    }
+
+    if (passwordConfirmation.value !== password.value) {
+        passwordConfirmation.parentElement.querySelector('p').classList.remove('hidden');
+        passwordConfirmation.parentElement.querySelector('p').classList.add('block');
+
+        passwordConfirmation.parentElement.querySelector('p').textContent = 'Passwords do not match';
+        return;
+    }
+
+    const data = {
+        email: email.value,
+        password: password.value,
+    };
+
+    localStorage.setItem('user', JSON.stringify(data));
+
+    movePage('login');
+}
+
+const checkDetailsForLogin = () => {
+    const email = document.getElementById('email-login');
+    const password = document.getElementById('password-login');
+
+    if (email.value === '') {
+        email.parentElement.querySelector('p').classList.remove('hidden');
+        email.parentElement.querySelector('p').classList.add('block');
+        return;
+    }
+
+    if (password.value === '') {
+        password.parentElement.querySelector('p').classList.remove('hidden');
+        password.parentElement.querySelector('p').classList.add('block');
+        return;
+    }
+
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (!user) {
+        movePage();
+        return;
+    }
+
+    if (user.email !== email.value) {
+        email.parentElement.querySelector('p').classList.remove('hidden');
+        email.parentElement.querySelector('p').classList.add('block');
+
+        email.parentElement.querySelector('p').textContent = 'Email does not exist';
+
+        movePage();
+        return;
+    }
+
+    if (user.password !== password.value) {
+        password.parentElement.querySelector('p').classList.remove('hidden');
+        password.parentElement.querySelector('p').classList.add('block');
+
+        password.parentElement.querySelector('p').textContent = 'Password is incorrect';
+        return;
+    }
+
+    user.loggedIn = true;
+    localStorage.setItem('user', JSON.stringify(user));
+
+    window.location.href = '../index.html';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const formSignUp = document.getElementById('signUpForm');
+    const formLogin = document.getElementById('loginForm');
+
+    const emailLogin = document.getElementById('email-login');
+    const passwordLogin = document.getElementById('password-login');
+
+    const emailSignUp = document.getElementById('email-signup');
+    const passwordSignUp = document.getElementById('password-signup');
+    const passwordConfirmation = document.getElementById('password-confirmation');
+
+    formLogin.addEventListener('submit', (e) => {
+        e.preventDefault();
+        checkDetailsForLogin();
+    });
+
+    emailLogin.addEventListener('keyup', () => {
+        if (emailLogin.value === '') {
+            emailLogin.parentElement.querySelector('p').classList.remove('hidden');
+            emailLogin.parentElement.querySelector('p').classList.add('block');
+        } else {
+            emailLogin.parentElement.querySelector('p').classList.remove('block');
+            emailLogin.parentElement.querySelector('p').classList.add('hidden');
+        }
+    });
+
+    passwordLogin.addEventListener('keyup', () => {
+        if (passwordLogin.value === '') {
+            passwordLogin.parentElement.querySelector('p').classList.remove('hidden');
+            passwordLogin.parentElement.querySelector('p').classList.add('block');
+        } else {
+            passwordLogin.parentElement.querySelector('p').classList.remove('block');
+            passwordLogin.parentElement.querySelector('p').classList.add('hidden');
+        }
+    });
+
+    formSignUp.addEventListener('submit', (e) => {
+        e.preventDefault();
+        checkDetailsForSignUp();
+    });
+
+    emailSignUp.addEventListener('keyup', () => {
+        if (emailSignUp.value === '') {
+            emailSignUp.parentElement.querySelector('p').classList.remove('hidden');
+            emailSignUp.parentElement.querySelector('p').classList.add('block');
+        } else {
+            emailSignUp.parentElement.querySelector('p').classList.remove('block');
+            emailSignUp.parentElement.querySelector('p').classList.add('hidden');
+        }
+    });
+
+    passwordSignUp.addEventListener('keyup', () => {
+        if (passwordSignUp.value === '') {
+            passwordSignUp.parentElement.querySelector('p').classList.remove('hidden');
+            passwordSignUp.parentElement.querySelector('p').classList.add('block');
+        } else {
+            passwordSignUp.parentElement.querySelector('p').classList.remove('block');
+            passwordSignUp.parentElement.querySelector('p').classList.add('hidden');
+        }
+    });
+
+    passwordConfirmation.addEventListener('keyup', () => {
+        const password = document.getElementById('password-signup');
+        const passwordConfirmation = document.getElementById('password-confirmation');
+
+        if (password.value === passwordConfirmation.value) {
+            passwordConfirmation.parentElement.querySelector('p').classList.remove('block');
+            passwordConfirmation.parentElement.querySelector('p').classList.add('hidden');
+        } else {
+            passwordConfirmation.parentElement.querySelector('p').classList.remove('hidden');
+            passwordConfirmation.parentElement.querySelector('p').classList.add('block');
+
+            passwordConfirmation.parentElement.querySelector('p').textContent = 'Passwords do not match';
+        }
+    });
+});
